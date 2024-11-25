db1f00fbabfab7140c6434dc262e5f16356f3882
Hajar <hajarmoubhale@gmail.com>
2024-11-25 09:40:53 +0100

    ahora bien con getter y stter

--- src/org/ies/highschool/model/Student.java
+++ src/org/ies/highschool/model/Student.java
@@ -1,23 +1,72 @@
 package org.ies.highschool.model;
 
+import java.util.Objects;
+
 public class Student {
-    // Campos
+
     private String name;
     private String surname;
     private String address;
 
-    // Constructor
+
     public Student(String name, String surname, String address) {
         this.name = name;
         this.surname = surname;
         this.address = address;
     }
 
-    // Método info
+
     public void info() {
         System.out.println("Información del Estudiante:");
         System.out.println("Nombre: " + name);
         System.out.println("Apellidos: " + surname);
         System.out.println("Dirección: " + address);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(surname, student.surname) && Objects.equals(address, student.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
 }
